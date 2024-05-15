33c2dbd8bccbc07b42a16d74b43b66e8392a494f
Deedeshoe <derrickdeshoe71@gmail.com>
2024-05-15 10:55:59 -0700

    first commit

---
+++ js/sandbox.js
@@ -0,0 +1,104 @@
+// const para =document.querySelectorAll('p');
+
+// para.forEach(p => {
+//     console.log(p.innerText);
+
+//     p.innerText +='Meek chimbonyarara!'
+// })
+
+// const text=document.getElementById("myself")
+// text.style.color="brown"
+
+// var name = "Derrick"
+//  alert(name)
+
+//  let surname ="Deshoe"
+
+//  console.log(surname)
+
+// //  const birthday ="22"
+
+
+// let a =5
+
+// let b =6
+
+// console.log(a+b)
+
+// var groceries =["sadza" , "banana" , "gaka"]
+
+// console.log(groceries)
+
+// // functon 
+// function sum(a,b,c) {
+//     return a-b+c
+// }
+
+// console.log(sum(5,8,9))
+
+// var name = prompt("What is your name?")
+
+// var surname = prompt("What is your surname?")
+
+// var dob = prompt("What is your date of birth?")
+
+// let sum = 2024-dob
+
+// console.log("HI! My name is " + name + " " + surname + ". I am " + sum + " years old. ")
+
+// for (
+
+//     var i=1; i<=10; i++)
+//     {console.log(i);}
+
+
+// var marks=prompt("Enter your marks")
+
+// if( marks>=80) { console.log("you passed")}
+
+// else{ console.log("you failed")}
+
+
+
+// for(
+//     let i=0; i<10; i++
+//     ) { 
+//         console.log(i)}
+
+/*While loop*/
+
+// let i=0
+// while(i<10)
+
+// {console.log(i); i++}
+
+
+
+// document.getElementById('click') .addEventListener('click', function() {
+//     document.getElementById('me') .innerHTML="Hello from Javascript"
+// })
+
+
+// document.getElementById("myform").addEventListener("submit", function(event){
+//    event.preventDefault();
+//    console.log("form submitted!");
+//  });
+
+
+// document.getElementById("myselect").addEventListener("change",function(event){
+// console.log("selected value;"+ event.target.value);
+// });
+
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+
+
+
